8ea81018667e85303858f0ec6025426abe24f20c
AngelCen14 <angelcen1447@gmail.com>
2024-11-05 21:15:23 +0100

    Refactorizacion

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -3,19 +3,36 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour {
-    
+    // References
     private InputActions _inputActions;
     
+    // Properties
     public Vector2 MovementInput { get; private set; }
     public Vector2 MousePosition { get; private set; }
     
+    #region Unity Methods
     private void Awake() {
         _inputActions = new InputActions();
         _inputActions.Player.Move.performed += OnMovePerformed;
         _inputActions.Player.Move.canceled += OnMoveCanceled;
         _inputActions.Player.Look.performed += OnLookPerformed;
     }
+    private void OnDestroy() {
+        _inputActions.Player.Move.performed -= OnMovePerformed;
+        _inputActions.Player.Move.canceled -= OnMoveCanceled;
+        _inputActions.Player.Look.performed -= OnLookPerformed;
+    }
 
+    private void OnEnable() {
+        _inputActions.Enable();
+    }
+
+    private void OnDisable() {
+        _inputActions.Disable();
+    }
+    #endregion
+
+    #region Input Events
     private void OnMovePerformed(InputAction.CallbackContext context) {
         MovementInput = context.ReadValue<Vector2>().normalized;
     }
@@ -27,17 +44,5 @@ public class GameInput : MonoBehaviour {
     private void OnLookPerformed(InputAction.CallbackContext context) {
         MousePosition = context.ReadValue<Vector2>();
     }
-
-    private void OnDestroy() {
-        _inputActions.Player.Move.performed -= OnMovePerformed;
-        _inputActions.Player.Look.performed -= OnLookPerformed;
-    }
-
-    private void OnEnable() {
-        _inputActions.Enable();
-    }
-
-    private void OnDisable() {
-        _inputActions.Disable();
-    }
+    #endregion
 }
--- Assets/Scripts/PlayerScripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerScripts/PlayerAnimation.cs
@@ -1,15 +1,18 @@
-using System;
 using UnityEngine;
 
 namespace PlayerScripts {
     [RequireComponent(typeof(Animator))]
     [RequireComponent(typeof(PlayerMovement))]
     public class PlayerAnimation : MonoBehaviour {
+        // Components
         private Animator _animator;
         private PlayerMovement _playerMovement;
-        
-        private readonly int _isMovingHash = Animator.StringToHash("IsMoving");
 
+        #region Animator Hashes
+        private readonly int _isMovingHash = Animator.StringToHash("IsMoving");
+        #endregion
+        
+        #region Unity Methods
         private void Awake() {
             _animator = GetComponent<Animator>();
             _playerMovement = GetComponent<PlayerMovement>();
@@ -18,5 +21,6 @@ namespace PlayerScripts {
         private void Update() {
             _animator.SetBool(_isMovingHash, _playerMovement.IsMoving());
         }
+        #endregion
     }
 }
